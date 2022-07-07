02eee00223c03a981968fa0d925f6c6f3d8bca66
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-07 22:51:41 +0200

    created preliminary form components & form stylesheet

---
+++ src/components/forms/auth-data-form.component.jsx
@@ -0,0 +1,12 @@
+import { FormHeader, FormSubHeader } from "./form.styles";
+
+const AuthDataForm = () => {
+  return (
+    <>
+      <FormHeader>Ahoy You!</FormHeader>
+      <FormSubHeader>Care to register?</FormSubHeader>
+    </>
+  );
+};
+
+export default AuthDataForm;
---
+++ src/components/forms/form.styles.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const FormHeader = styled.h1`
+  font-family: "Eczar";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 40px;
+  line-height: 102%;
+  text-align: center;
+  letter-spacing: -0.01em;
+  color: #343541;
+`;
+
+export const FormSubHeader = styled(FormHeader)`
+  color: #85868d;
+`;

cb70032d4ffcaa72415262c3a50b4a1368f71008
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-07 22:45:17 +0200

    temporarily removed MonteTheme styled-components theme provider

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import "./index.css";
 
 import { FormProvider } from "./context/form.context";
 
+// import MonteTheme from "./utils/styled-components/monte-theme.theme";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <FormProvider>
+      {/* <MonteTheme> */}
       <App />
+      {/* </MonteTheme> */}
     </FormProvider>
   </React.StrictMode>
 );

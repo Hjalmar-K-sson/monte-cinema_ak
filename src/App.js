2109d50f31c65c2b06ea2be2f70cfb2583d626a6
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-09 20:29:59 +0200

    imported and added FormStepWrapper component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from "../src/components/header/header.component.jsx";
-import AuthDataForm from "./components/forms/auth-data-form.component.jsx";
+import FormStepWrapper from "./components/form-step-wrapper/FormStepWrapper.component.jsx";
 
 function App() {
   return (
     <>
       <Header />
-      <AuthDataForm />
+      <FormStepWrapper />
     </>
   );
 }

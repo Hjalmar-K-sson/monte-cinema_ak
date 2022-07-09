b85e5ca10aaffc31198df1531990ee2b0165be9e
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-09 20:33:13 +0200

    added form data tracking to form context provider

--- src/context/form.context.jsx
+++ src/context/form.context.jsx
@@ -3,13 +3,18 @@ import { createContext, useState } from "react";
 export const FormContext = createContext({
   activeStepId: 0,
   setActiveStepId: () => {},
+  formData: {},
+  setFormData: () => {},
 });
 
 export const FormProvider = ({ children }) => {
   const [activeStepId, setActiveStepId] = useState(0);
+  const [formData, setFormData] = useState({});
   const value = {
     activeStepId,
     setActiveStepId,
+    formData,
+    setFormData,
   };
   return <FormContext.Provider value={value}>{children}</FormContext.Provider>;
 };

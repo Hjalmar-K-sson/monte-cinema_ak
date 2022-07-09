2d2f98cd969e2a5699b29171254a42ef135e4f53
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-09 20:29:18 +0200

    crated FormStepWrapper component for rendering form steps depending on active step index

---
+++ src/components/form-step-wrapper/FormStepWrapper.component.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import { FormContext } from "../../context/form.context";
+
+import AuthDataFormStep from "../form-steps/auth-data-form-step.component";
+import PersonalDataFormStep from "../form-steps/personal-data-form-step.component ";
+import FormSubmitSuccessStep from "../form-steps/form-submit-success-step.component";
+
+const FormStepWrapper = () => {
+  const { activeStepId } = useContext(FormContext);
+  let formStep;
+
+  switch (activeStepId) {
+    case 0:
+      formStep = <AuthDataFormStep />;
+      break;
+    case 1:
+      formStep = <PersonalDataFormStep />;
+      break;
+    case 2:
+      formStep = <FormSubmitSuccessStep />;
+      break;
+    default:
+      break;
+  }
+  return formStep;
+};
+
+export default FormStepWrapper;

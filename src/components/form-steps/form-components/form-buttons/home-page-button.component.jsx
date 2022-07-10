97be226cb8aed66ac2178012d6ff7693ff25b94e
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-10 21:28:46 +0200

    created home page homepage button

---
+++ src/components/form-steps/form-components/form-buttons/home-page-button.component.jsx
@@ -0,0 +1,7 @@
+import { HomepageLinkButton } from "./form-buttons.styles";
+
+const HomePageButton = ({ href, buttonText }) => {
+  return <HomepageLinkButton>{buttonText}</HomepageLinkButton>;
+};
+
+export default HomePageButton;

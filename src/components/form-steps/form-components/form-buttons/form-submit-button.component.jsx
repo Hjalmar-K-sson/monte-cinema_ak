092bdf47a075724d2f00ecefca889e96e4f694a9
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-10 21:27:58 +0200

    created form submit button

---
+++ src/components/form-steps/form-components/form-buttons/form-submit-button.component.jsx
@@ -0,0 +1,7 @@
+import { SubmitButton } from "./form-buttons.styles";
+
+const FormSubmitButton = ({ buttonText }) => {
+  return <SubmitButton type="submit">{buttonText}</SubmitButton>;
+};
+
+export default FormSubmitButton;

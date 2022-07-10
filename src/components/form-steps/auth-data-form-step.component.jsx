6963bc9916280f61eabb990dcc8d683a2a3c7258
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-10 21:10:46 +0200

    removed unused useState hook import, removed unused styled components & components, removed unused password visibility logic, imported new password form field & button components, updated form structure

--- src/components/form-steps/auth-data-form-step.component.jsx
+++ src/components/form-steps/auth-data-form-step.component.jsx
@@ -1,17 +1,14 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 import { FormContext } from "../../context/form.context";
 
 import FormHeading from "./form-components/form-heading/form-heading.component";
 import TextFormField from "./form-components/form-fields/text-form-field.component";
-import FormButtonGroup from "./form-components/form-button-group/form-button-group.component";
+import PasswordFormField from "./form-components/form-fields/password-form-field.component";
+import FormSubmitButton from "./form-components/form-buttons/form-submit-button.component";
+import LoginInsteadButton from "./form-components/form-buttons/login-instead-button.component";
 
-import {
-  ContentContainer,
-  FormContainer,
-  StyledForm,
-  ToggleVisibilityButton,
-} from "./form.styles";
+import { ContentContainer, StyledForm, FormButtonWrapper } from "./form.styles";
 
 import { Formik } from "formik";
 import { authFormValidationSchema } from "../../utils/yup-utils/form-validation.schema";
@@ -20,52 +17,45 @@ const AuthDataFormStep = () => {
   const { activeStepId, setActiveStepId, formData, setFormData } =
     useContext(FormContext);
 
-  const [passwordVisible, setPasswordVisible] = useState(false);
-  const togglePasswordVisible = () => setPasswordVisible(!passwordVisible);
-
   return (
     <ContentContainer>
       <FormHeading
         headerText={"Ahoy You!"}
         subHeaderText={"Care to register?"}
       />
-      <FormContainer>
-        <Formik
-          initialValues={{
-            email: "",
-            password: "",
-          }}
-          validationSchema={authFormValidationSchema}
-          onSubmit={(values) => {
-            const data = { ...formData, ...values };
-            console.log(data);
-            setFormData(data);
-            setActiveStepId(activeStepId + 1);
-          }}
-        >
-          <StyledForm>
-            <TextFormField
-              label="email"
-              name="email"
-              type="email"
-              placeholder="email"
-            />
-            <TextFormField
-              label="password"
-              name="password"
-              type="password"
-              placeholder="password"
-            >
-              <ToggleVisibilityButton onClick={togglePasswordVisible} />
-            </TextFormField>
-            <FormButtonGroup
-              submitButtonText="Next step"
-              loginButtonText="Log in instead"
-            />
-            {/* <ToggleVisibilityIcon /> */}
-          </StyledForm>
-        </Formik>
-      </FormContainer>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={authFormValidationSchema}
+        onSubmit={(values) => {
+          const data = { ...formData, ...values };
+          console.log(data);
+          setFormData(data);
+          setActiveStepId(activeStepId + 1);
+        }}
+      >
+        <StyledForm>
+          <TextFormField
+            label="email"
+            name="email"
+            type="email"
+            placeholder="Something ending with monterail.com"
+            required
+          />
+          <PasswordFormField
+            label="password"
+            name="password"
+            placeholder="Enter your password"
+            required
+          />
+          <FormButtonWrapper>
+            <FormSubmitButton buttonText="Next step" />
+            <LoginInsteadButton href="#" buttonText="Log in instead" />
+          </FormButtonWrapper>
+        </StyledForm>
+      </Formik>
     </ContentContainer>
   );
 };

162c2b56915a2fc7e2375556d1c49d246055f8e3
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-10 21:07:28 +0200

    uncommented temporarily unused PersonalDataFormStep import

--- src/components/form-step-wrapper/FormStepWrapper.component.jsx
+++ src/components/form-step-wrapper/FormStepWrapper.component.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { FormContext } from "../../context/form.context";
 
 import AuthDataFormStep from "../form-steps/auth-data-form-step.component";
-import PersonalDataFormStep from "../form-steps/personal-data-form-step.component ";
+import PersonalDataFormStep from "../form-steps/personal-data-form-step.component";
 import FormSubmitSuccessStep from "../form-steps/form-submit-success-step.component";
 
 const FormStepWrapper = () => {

2b447e1f940842d4c9b15e17e17493bb62b5ce58
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-10 21:04:46 +0200

    removed unnecessary imports & styled components, updated remaining styled components with theme props

--- src/components/form-steps/form.styles.jsx
+++ src/components/form-steps/form.styles.jsx
@@ -1,10 +1,5 @@
 import styled from "styled-components";
 import { Form } from "formik";
-import { ReactComponent as ToggleVisibilityIcon } from "../../assets/eye.svg";
-
-export const ToggleVisibilityButton = styled(ToggleVisibilityIcon)`
-  position: absolute;
-`;
 
 export const ContentContainer = styled.div`
   display: flex;
@@ -12,29 +7,8 @@ export const ContentContainer = styled.div`
   justify-content: flex-start;
   align-items: center;
   padding: 0px;
-  position: relative;
   width: 100vw;
-  /* height: 100vh; */
-  background: #ffffff;
-  @media (min-width: 1024px) {
-    justify-content: center;
-  }
-`;
-
-export const FormContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  /* padding: 0 1.5em; */
-  @media (min-width: 1024px) {
-    box-shadow: 0px 4px 22px rgba(52, 53, 65, 0.15);
-    border-radius: 24px;
-    width: 65%;
-  }
-  @media (min-width: 1440px) {
-    width: 45%;
-  }
+  background: ${(props) => props.theme.colors.snowWhite};
 `;
 
 export const StyledForm = styled(Form)`
@@ -42,61 +16,36 @@ export const StyledForm = styled(Form)`
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-`;
-
-export const FormField = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-start;
-`;
-
-export const FormLabel = styled.p`
-  font-family: "Roboto Mono";
-  font-style: normal;
-  font-weight: 700;
-  font-size: 14px;
-  line-height: 18px;
-  text-transform: uppercase;
-  padding: 0.5em;
-  color: #f47073;
-`;
-
-export const FormInput = styled.input`
-  height: 1.4em;
-  background: #f7f7f7;
-  border: none;
-  border-radius: 8px;
-  font-family: "Roboto";
-  font-style: normal;
-  font-weight: 400;
-  font-size: 18px;
-  line-height: 21px;
-  padding: 0.8em;
-  color: #85868d;
-`;
-
-export const SubmitButton = styled.button`
-  display: block;
-  padding: 1.2em 7.1em;
-  background: #ec1115;
-  border-radius: 64px;
-  font-family: "Roboto Mono";
-  font-style: normal;
-  font-weight: 500;
-  font-size: 18px;
-  line-height: 100%;
-  letter-spacing: 0.015em;
-  color: #ffffff;
+  width: 100%;
+  padding: 1.5rem;
+  @media (min-width: 1024px) {
+    box-shadow: ${(props) => props.theme.shadows.formShadow};
+    border-radius: ${(props) => props.theme.radiuses.formContRadius};
+    width: 37.5rem;
+    padding: 4rem;
+    margin-bottom: 10rem;
+  }
 `;
 
 export const SuccessParagraph = styled.div`
   padding: 0 1.5em;
-  font-family: "Roboto";
+  font-family: ${(props) => props.theme.fonts.defaultFont};
   font-style: normal;
-  font-weight: 400;
-  font-size: 18px;
+  font-weight: ${(props) => props.theme.fontWeights.sm};
+  font-size: ${(props) => props.theme.fontSizes.md};
   line-height: 21px;
   text-align: center;
-  color: #292a33;
+  color: ${(props) => props.theme.colors.grayCharade};
+`;
+
+export const FormButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 7.5rem;
+  @media (min-width: 1024px) {
+    flex-direction: row;
+  }
 `;

551d336b7703f958f3516ad88e474817b2b4548e
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-11 22:54:26 +0200

    added missing href attribute to homepage link button

--- src/components/form-steps/form-components/form-buttons/home-page-button.component.jsx
+++ src/components/form-steps/form-components/form-buttons/home-page-button.component.jsx
@@ -1,7 +1,7 @@
 import { HomepageLinkButton } from "./form-buttons.styles";
 
 const HomePageButton = ({ href, buttonText }) => {
-  return <HomepageLinkButton>{buttonText}</HomepageLinkButton>;
+  return <HomepageLinkButton href={href}>{buttonText}</HomepageLinkButton>;
 };
 
 export default HomePageButton;

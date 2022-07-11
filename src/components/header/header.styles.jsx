d73fd0e5cbe9371b4a794dc355dbd46af8aee834
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-07-11 23:02:17 +0200

    cleaned up styling

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,27 +1,5 @@
 import styled from "styled-components";
 
-export const HeaderContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  svg {
-    padding: 3em;
-    display: block;
-  }
-  span {
-    display: none;
-    padding: 3em;
-    @media (min-width: 1024px) {
-      display: block;
-    }
-  }
-
-  @media (min-width: 1024px) {
-    justify-content: space-between;
-  }
-`;
-
 export const HeaderSlogan = styled.span`
   font-family: ${(props) => props.theme.fonts.labelFont};
   font-style: normal;
@@ -29,7 +7,29 @@ export const HeaderSlogan = styled.span`
   font-size: ${(props) => props.theme.fontSizes.sm};
   line-height: 21px;
   text-align: right;
-  letter-spacing: 0.03em;
+  letter-spacing: 0.03rem;
   text-transform: uppercase;
   color: ${(props) => props.theme.colors.grayTuna};
+`;
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  max-width: 100%;
+  @media (min-width: 1024px) {
+    justify-content: space-between;
+  }
+  svg {
+    padding: 3rem;
+    display: block;
+  }
+  ${HeaderSlogan} {
+    display: none;
+    padding: 3rem;
+    @media (min-width: 1024px) {
+      display: block;
+    }
+  }
 `;
